c0a6f6f4248652981e2ac016487c16c4c0fd443c
hm <mark.huang1212@gmail.com>
2021-05-09 17:54:22 +0800

    0.0.1

--- src/Config.ts
+++ src/Config.ts
@@ -1,15 +1,15 @@
 class Config {
 
-    static shared = new Config()
+    static shared = new Config()                // DO NOT CHANGE THIS LINE
 
-    afs_path = '/tos/bin/tfs-x86_64b'
+    afs_path = '/tos/bin/tfs-x86_64b'           // path of the executable of tfc-x86_64b
 
-    chain_endpoint = 'http://localhost:8545'
-    chain_tfc_addr = ''
+    chain_endpoint = 'http://localhost:8545'    // endpoint of the Tfc-Chain
+    chain_tfc_addr = ''                         // address of the Tfc-Chain contract
 
-    rnode_privkey = ''
-    verifier_prikey = ''
-    seed_submitter_privkey = ''
+    rnode_privkey = ''                          // private key with RNode Role
+    verifier_prikey = ''                        // private key with Verifier Role
+    seed_submitter_privkey = ''                 // private key with SeedSubmitter Role
 
 }
 
