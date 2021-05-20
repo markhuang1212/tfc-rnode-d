48ae242eaa953f31f41f62f6628909b9618d7655
hm <mark.huang1212@gmail.com>
2021-05-20 20:10:59 +0800

    de

--- src/chain/VerifierShared.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const adapter_1 = require("@tfc-chain/adapter");
-const Config_1 = __importDefault(require("../Config"));
-const VerifierShared = new adapter_1.Verifier(Config_1.default.shared.chain_endpoint, Config_1.default.shared.verifier_prikey, Config_1.default.shared.chain_tfc_addr);
-exports.default = VerifierShared;
--- src/cli/CGenerateProof.ts
+++ src/cli/CGenerateProof.ts
@@ -16,12 +16,13 @@ class CGenerateProof {
 
     async generateProof(sector_afid: string, check_afid: string) {
         const { stdout } = await exec(`${this.afs_path} ";_f=generate_proof;sector_afid=${sector_afid};check_afid=${check_afid};"`)
-        // console.log(stdout)
         if (stdout.match(/_r=true/) && stdout.match(/proof_afid=[0-9,a-z,A-Z]*/)) {
             const proof_afid = stdout.match(/proof_afid=[0-9,a-z,A-Z]*/)![0].substr(11)
             return { proof_afid }
         }
+        console.log(stdout)
         throw Error('Unexpected CLI behavior')
+
     }
 
 }
